a447f7471360d685bc5c774a1d9cb3ed8dbb820e
Eyobedk <Eyobedkebede10@gmail.com>
2022-08-11 10:40:05 +0100

    app component to render notebox comp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Notebox from './Notebox';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="cards">
+         <h2 className="header">
+            MY NOTE BOOKS
+         </h2>
+
+          <Notebox />
+      </div>
   );
 }
 
